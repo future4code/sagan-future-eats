1e96dc775ad6e0c431ad9439658bb87bff759994
Danilo Mourelle <danilomourelle@outlook.com>
2020-04-14 17:05:14 -0300

    configuração dos arquivos e rotas

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -17,7 +17,6 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { generateReducers } from "../../reducers";
 import { routerMiddleware } from "connected-react-router";
 import { createGlobalStyle } from 'styled-components'
-import { StylesProvider } from '@material-ui/core/styles';
 
 const generateClassName = createGenerateClassName();
 const jss = create({
---
+++ src/containers/ProfilePage/editProfile.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import MyBottonNav from '../../components/material/BottomNav'
+import MyButton from '../../components/material/Button'
+import { MyInput, MyPasswordInput } from '../../components/material/Inputs'
+
+const PageWrapper = styled.div`
+  width: 100%;
+`
+
+export class ProfileEdit extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <PageWrapper>
+        <MyInput label='Endereço' placeholder="Av. / Rua" />
+        <MyPasswordInput label='Senha' />
+        <MyButton btnText='Confirmar' />
+        <MyBottonNav />
+      </PageWrapper>
+    )
+  }
+}
+
+export default ProfileEdit
